58e1c11eca6cf368f717935281f17a8d63f441e2
Jarmo van der Heul <jarmovdh@MacBook-Pro-van-Jarmo.local>
2020-04-30 12:08:56 +0200

    menu changes

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+}
 
-
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
 
 //clock
 function startTime() {
